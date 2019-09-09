af5b84a49966bb11d20827137962b6208ebaea8b
Kabir Oberoi <kabiroberoi@dhcp-wifi-8021x-168-122-5-190.bu.edu>
2019-09-09 17:40:55 -0400

    version2

--- safety/firestation.py
+++ safety/firestation.py
@@ -1,4 +1,20 @@
-def draw_firestation():
+def draw_firestation(no_building):
     #testing GutHub
-    print("fire station found")
+    
+    # building supplies aka variables
+    roof = "----"
+    pillars = "|  |"
+    floor = "----"
+
+    if no_building < 0:
+        raise ValueError("Number of buildings must be positive")
+
+    else:
+    #print("fire station found")
+        print(roof*no_building)
+        print(pillars*no_building)
+        print(floor*no_building)
+
     return
+
+draw_firestation(2)
